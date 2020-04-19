02bd63d9b3ee1abfb9c4860a0862389fdbbe3f08
pjay79 <praveen.jayarajan@gmail.com>
2020-04-19 13:57:45 +1000

    Config edits

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
     <NavigationContainer>
       <View style={styles.container}>
         <Text style={styles.welcome}>TeleHealth App</Text>
-        <Text style={styles.instructions}>Version 1.0.0</Text>
+        <Text style={styles.instructions}>V 1.0.0</Text>
     </View>
     </NavigationContainer>
   );
